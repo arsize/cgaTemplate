2b102a4421bbbe5e13f9911366aafc60dcae7de9
arsize <arsizeof3416@gmail.com>
2021-07-05 11:09:47 +0800

    fix:zip

---
+++ run.js
@@ -0,0 +1,13 @@
+// read catchlist<catch.txt
+// zip -qr vue2_default.zip ./vue2_default
+// mv ./vue2_default.zip /usr/template/package/zip/vue2_default.zip
+
+const fs = require("fs")
+const shell = require("shelljs")
+
+var text = fs.readFileSync("./catch.txt", "utf-8")
+text = text.split(";")
+text.map(res => {
+    shell.exec(`zip -qr ${res}.zip ./${res}`)
+})
+console.log(text)
