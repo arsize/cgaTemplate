9189ae3ddf4119171c712c49948c7c069e3bbdba
arsize <arsizeof3416@gmail.com>
2021-07-05 14:33:47 +0800

    del:husky

--- build.js
+++ build.js
@@ -7,20 +7,18 @@ fs.writeFileSync('./catch.txt', '', "UTF-8")
 async function printgit() {
     const repo = await Git.Repository.open(path.resolve(__dirname, '.git'));
     const diff = await Git.Diff.indexToWorkdir(repo, null, {
-        flags: Git.Diff.OPTION.SHOW_BINARY
+        flags: Git.Diff.OPTION.SHOW_UNTRACKED_CONTENT | Git.Diff.OPTION.RECURSE_UNTRACKED_DIRS
     });
     diff.patches().then((patches) => {
         let temp = []
         patches.forEach((patch) => {
             let pth = patch.newFile().path()
-            console.log(pth)
             if (pth.indexOf("/") != -1 && pth.indexOf(".github/") == -1 && pth.indexOf(".husky/") == -1) {
                 if (!temp.includes(pth.split("/")[0])) {
                     temp.push(pth.split("/")[0])
                 }
             }
         });
-        return
         temp.map(res => {
             fs.appendFileSync("./catch.txt", res + ";", "UTF-8", { 'flags': 'a+' })
         })
@@ -29,4 +27,4 @@ async function printgit() {
     return diff
 }
 
-// printgit().then(res => { })
+printgit().then(res => { })
