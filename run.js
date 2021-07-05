f8c1b605c515ba625fe5ab18dbfce0014b63374d
arsize <arsizeof3416@gmail.com>
2021-07-05 11:29:19 +0800

    mv

--- run.js
+++ run.js
@@ -8,5 +8,5 @@ var text = fs.readFileSync("./catch.txt", "utf-8")
 text = text.split(";")
 text.map(res => {
     shell.exec(`zip -qr ${res}.zip ./${res}`)
-    shell.exec(`mv /usr/template/package/zip/${res}.zip ./${res}.zip`)
+    shell.exec(`mv ./${res}.zip /usr/template/package/zip/${res}.zip`)
 })
